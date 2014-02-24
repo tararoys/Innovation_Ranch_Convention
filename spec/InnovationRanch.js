77b91731e022999405999abd09ea0222a732a89e
Tara Roys <tlroys@gmail.com>
2014-02-24 00:29:24 -0600

    Short  call to action

---
+++ spec/InnovationRanch.js
@@ -0,0 +1,24 @@
+describe("Dev BootCamp Spring Breakout Session", function(){
+  it("Most convincing reaason to go: It is important to teach students that you don't need an office to be a developer", function(){
+    answer = null
+    expect(answer).toEqual("true")
+  }) 
+  it("Most important concern: can students learn as much about development at the Innovation Ranch as they could staying in Chicago?", function(){
+    answer = null
+    expect(answer.toEqual("true"))
+  })
+  it("It's cold in Chicago. I could use some sunshine", function(){
+    answer = null
+    expect(answer.toEqual("true"))
+  })
+}); 
+
+describe("Logistics", function(){
+  
+  
+})
+
+// describe("X", function(){
+
+
+// }); 
