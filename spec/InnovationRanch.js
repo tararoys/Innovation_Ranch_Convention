7256fd901903982b226c469b85764fd008c69b30
Tara Roys <tlroys@gmail.com>
2014-02-26 08:12:53 -0600

    Removed example files
    Removed example files from jasmine skeleton

--- spec/InnovationRanch.js
+++ spec/InnovationRanch.js
@@ -14,9 +14,72 @@ describe("Dev BootCamp Spring Breakout Session", function(){
 }); 
 
 describe("Logistics", function(){
-  
-  
+  describe("Housing", function(){
+    it("What are the sleeping arrangements at the Innovation Ranch?", function(){
+      var cost = "free";
+      var bring_sleeping_bags = null;
+      var bring_tents = null;
+      var there_are_beds_and_couches = null;
+      var there_is_plenty_of_floor_space = null;
+      var its_like_burning_man_for_tech_people = null;
+
+      var made_everything_in_this_section_not_equal_null = false
+      expect(made_everything_in_this_section_not_equal_null).toEqual("true");
+    })
+  })
+  describe("Transportation", function(){
+    it("By Car", function(){
+      var cost_in_dollars_for_car_round_trip = 500.00 //cost in dollars for 1 car in gasoline round trip according to the AA calculator http://fuelcostcalculator.aaa.com/
+      var number_of_hours_to_drive_one_way = 17.6;
+      var number_of_drivers_needed_to_drive_continuously = 2;
+      var number_of_cars_needed_for_all_of_dev_bootcamp = 80.0/4;
+      var ballpark_cost_per_person_in_dollars = 125;
+      var made_everything_in_this_section_not_equal_null = true;
+      expect(made_everything_in_this_section_not_equal_null).toEqual(true);
+    });
+    it("By Train", function(){
+      var amtrack_train_name = "Texas Eagle";
+      var number_of_hours_on_train = 28;
+      var ballpark_minium_cost_per_person = 300;
+      var ballpark_maximum_cost_per_person = 800;
+      var ability_to_rent_whole_cars_for_dbc_students = null;
+      var ability_to_charter_a_train = null;
+
+      var made_everything_in_this_section_not_equal_null = false;
+      expect(made_everything_in_this_section_not_equal_null).toEqual(true);
+
+    });
+    it("By Zepplin", function(){
+      var sadly_there_is_only_one_and_it_only_fits_ten_people = true;
+      var its_a_shame = sadly_there_is_only_one_and_it_only_fits_ten_people
+      expect(its_a_shame).toEqual(true);
+      //http://milwaukee.about.com/od/festivalsevents/qt/Zeppelin-Takes-To-Milwaukee-Skies.htm
+    });
+  })
 })
+
+
+ // 
+ //    
+ //    it("By Zepplin," function(){
+ //      var sadly_there_is_only_one_and_it_only_fits_ten_people = true;
+ //      //http://milwaukee.about.com/od/festivalsevents/qt/Zeppelin-Takes-To-Milwaukee-Skies.htm
+ //    });
+ //  })
+
+ //  describe("Food", function(){
+ //    it("Plan #1: Trade wide-eyed enthusiam for food strategy", function(){
+ //      var will_this_work_for_70_people = null
+ //    });
+ //    it("Plan #2: Buy it", function(){
+ //      var dollars_per_person_per_day = 30
+ //      var dollars_per_person_10_days = 130
+ //    });
+ //    it("Plan #3: bring it", function(){
+ //      var have_sam_make_us_tons_of_sauce_and_noodles = null;
+ //    });
+ //  })
+
 
 // describe("X", function(){
 
